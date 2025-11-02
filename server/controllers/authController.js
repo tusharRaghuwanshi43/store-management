610bf5eb3c66ea4d050f80b697f80125bc769183
tusharraghuwanshi43 <tusharraghuwanshi82@gmail.com>
2025-11-02 19:48:41 +0530

    issues api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-const Admin = require('../models/Admin');
+const Admin = require('../models/admin');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // Add validation helper functions
