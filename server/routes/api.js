a691c3a4cdf21ecd088d48d5eb124512d8b8be79
tusharraghuwanshi43 <tusharraghuwanshi82@gmail.com>
2025-11-02 17:45:50 +0530

    updated api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const verifyAdminToken = require('../middleware/authMiddleware');
 
 // Product API routes
-router.get('/products', productController.getAllProducts);
-router.post('/addProduct', productController.createProduct);
-router.delete('/products/:id', productController.deleteProduct);
-router.put('/products/:id', productController.updateProduct);
+router.get('/products', verifyAdminToken, productController.getAllProducts);
+router.post('/products', verifyAdminToken, productController.createProduct);
+router.put('/products/:id', verifyAdminToken, productController.updateProduct);
+router.delete('/products/:id', verifyAdminToken, productController.deleteProduct);
 
 module.exports = router;
 
